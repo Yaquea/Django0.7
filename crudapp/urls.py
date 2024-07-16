f02b0f26b16bbf139d6ad0cc0fbe65e175e40263
Yaquea <gerardoyou30@gmail.com>
2024-07-15 20:01:03 -0600

    Registro de usuario

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Registro, name='Registro')
+    path('', views.Main, name='Main'),
+    path('Registro/', views.Registro, name='Registro')
 ]
--- crudapp/views.py
+++ crudapp/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+#Main menu
+def Main(request):
+    return render(request, 'Main.html', {})
 
+
+#Registro
 def Registro(request):
-    return render(request, 'Registro.html')
+
+    if request.method == ('GET'):
+        return render(request, 'Registro.html', 
+                  {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username= request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return redirect('Main')
+            except:
+                Error= 'Usuario ya existe'
+            return render(request, 'Registro.html', 
+                  {'form': UserCreationForm, 'Error':Error})
+        else:
+            Error= 'Las password no coinciden'
+            return render(request, 'Registro.html', 
+                  {'form': UserCreationForm, 'Error':Error})
+            
