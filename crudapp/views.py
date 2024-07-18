fd395b3ef6b8f6f4a9cdbc96886429c927f05e1a
Yaquea <gerardoyou30@gmail.com>
2024-07-17 18:58:44 -0600

    Extends (jinja)

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 #Main menu
 def Main(request):
     return render(request, 'Main.html', {})
@@ -17,8 +19,9 @@ def Registro(request):
             try:
                 user = User.objects.create_user(username= request.POST['username'], password=request.POST['password1'])
                 user.save()
+                login(request, user)
                 return redirect('Main')
-            except:
+            except IntegrityError:
                 Error= 'Usuario ya existe'
             return render(request, 'Registro.html', 
                   {'form': UserCreationForm, 'Error':Error})
@@ -26,4 +29,6 @@ def Registro(request):
             Error= 'Las password no coinciden'
             return render(request, 'Registro.html', 
                   {'form': UserCreationForm, 'Error':Error})
+        
+#Inicio de sesion
             
